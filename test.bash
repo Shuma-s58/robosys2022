d83333a9cf0d009d59cc31a08ce8861b4260e835
Shuma Suzuki <shuma.suzuki.2021@outlook.jp>
2022-10-27 12:59:03 +0900

    plus test end

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Shuma Suzuki
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -11,8 +11,16 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-
 [ "${out}" = 15 ]  || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(seq あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(seq | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
