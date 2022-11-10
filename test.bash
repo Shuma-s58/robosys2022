795323670108748cc1fdf54ae7501c7914306a4a
Shuma Suzuki <shuma.suzuki.2021@outlook.jp>
2022-11-10 18:20:05 +0900

    plus test

--- test.bash
+++ test.bash
@@ -11,7 +11,7 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ]  || ng ${LINENO}
+[ "${out}" = "すべて足した合計 = 15" ]  || ng ${LINENO}
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
