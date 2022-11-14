e3067995b03529d435e5e4ce72c064bebe9a77a1
Shuma Suzuki <shuma.suzuki.2021@outlook.jp>
2022-11-14 20:22:20 +0900

    plus test

--- test.bash
+++ test.bash
@@ -66,6 +66,10 @@ out=$(echo / | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
+out=$(echo = | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
